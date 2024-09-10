6622b9f5c2bd6fb5bfdbc3654dbe7b9bcaf21d69
J-b-benetti <j.b.benetti81@gmail.com>
2024-09-10 14:36:37 +0200

    Form for Medication Administration is completed

--- PM/src/app/app.component.ts
+++ PM/src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MedicationFormComponent } from './medication-form/medication-form.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule, FormsModule, MedicationFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'PM';
+  title = 'Medication Administration';
 }
